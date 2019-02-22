326ed386b2968c7bcb09a77c24e98349769e1803
SABLERY edouard <sablery@informatique.univ-paris-diderot.fr>
2019-02-22 14:33:30 +0100

    Instructions pour ajouter des commandes utilisateur reconnues par le parser.

--- userCommands.c
+++ userCommands.c
@@ -3,6 +3,11 @@
 
 // Fonctions reconnues par le parser
 // Doivent pouvoir recevoir tous ses paramètres sous forme de string aka (char*)
+// pour rajouter une commande dans le parser :
+//    1: - ajouter une fonction intermédiaire qui prendra les paramatres sous forme de char*, les tranformera dans le bon types puis appelera la fonction voulu
+//    2: - mettre sa signature dans userCommands.h
+//    3: - dans la fonction initCommands créer un pointeur de la fonction intermédiaire créée plus tot puis créer un struc fonction efin  Rajouter ce struct fonction dans le tableau res
+//    4: - Pour finir il faut mettre à jour le nombre de fonction dans le fichier parser.h dans les DEFINE
 
 int test1(char* entier) {
   printf("\nExecution of test1 with %s",entier);
@@ -14,6 +19,8 @@ int test2(char* var, char* entier) {
   return 0;
 }
 
+//initCommands est utilisé par le parser elle permet de lui donner les fonctions
+//Fonction qui initialise un tableau de struct fonction  passé en parametre
 void initCommands(fonction* res) {
   //test1 function
   int(*pointeurTest1)(char*);
